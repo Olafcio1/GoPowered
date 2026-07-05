eaa3417788463d7893f6908d9ebf6cbbea9eeb15
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-07-05 14:25:39 +0200

    Implicit struct values

--- Lang/Parser/Parser.cs
+++ Lang/Parser/Parser.cs
@@ -1350,6 +1350,39 @@ namespace GoPowered.Lang.Parser
                 var body = ParseCode();
                 return new ETClosure(args, body, returns, generics);
             }
+            else if (Now([(null, Operator.LCurly.ToToken())], true))
+            {
+                var token = new ETImplicitStruct([]);
+
+                var comma = false;
+                var newlines = false;
+
+                while (true)
+                {
+                    ConsumeNewlines(ref newlines);
+
+                    if (!newlines && Now([(null, Operator.RCurly.ToToken())], true))
+                        break;
+                    else if (comma)
+                        Require(Operator.Comma.ToToken(), "','");
+                    else comma = true;
+
+                    ConsumeNewlines(ref newlines);
+
+                    if (Now([(null, Operator.RCurly.ToToken())], true))
+                        break;
+
+                    ConsumeNewlines(ref newlines);
+
+                    var key = Consume<LTLiteral>().Value;
+                    Require(Operator.Colon.ToToken(), "':'");
+                    var value = ParseExpression();
+
+                    token.Fields[key] = value;
+                }
+
+                return token;
+            }
             else if (Now([(null, Keyword.MAP.ToToken())], true))
             {
                 Require(Operator.LSquare.ToToken(), "'['");
---
+++ Lang/Parser/Token/Expr/Target/ETImplicitStruct.cs
@@ -0,0 +1,5 @@
+﻿namespace GoPowered.Lang.Parser.Token.Expr.Target
+{
+    public record ETImplicitStruct(Dictionary<string, IAnyExpression> Fields)
+                : IExpressionTarget;
+}
