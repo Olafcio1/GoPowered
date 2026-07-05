df5b85c96b3605f08dc24c6f9b8ce73b36de6a46
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-07-05 11:24:50 +0200

    Fix ESTNil serialization stackoverflow

--- Lang/Parser/Token/Expr/Target/Single/ESTNil.cs
+++ Lang/Parser/Token/Expr/Target/Single/ESTNil.cs
@@ -1,6 +1,6 @@
 ﻿namespace GoPowered.Lang.Parser.Token.Expr.Target.Single
 {
-    public record ESTNil : IExpressionTarget
+    public record ESTNil : IExpressionTarget, IAvoidSerialization
     {
         public static readonly ESTNil INSTANCE = new ESTNil();
         private ESTNil() {}
