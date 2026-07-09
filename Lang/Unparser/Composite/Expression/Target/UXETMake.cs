bb33724fedaf1d6fb9cb9b43bcc85841507ed604
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-07-09 23:15:52 +0200

    [Unparser] [UXExpression] Unparsing 'ETMake'

---
+++ Lang/Unparser/Composite/Expression/Target/UXETMake.cs
@@ -0,0 +1,29 @@
+﻿using GoPowered.Lang.Parser.Token.Expr;
+using GoPowered.Lang.Parser.Token.Expr.Target;
+
+namespace GoPowered.Lang.Unparser
+{
+    public partial class Unparser
+    {
+        protected partial bool HandleMake(IExpressionTarget target, ref string output)
+        {
+            if (target is ETMake cast)
+            {
+                output += "make(";
+                output += HandleType(cast.Type);
+
+                foreach (var arg in cast.Values)
+                {
+                    output += ", ";
+                    output += HandleAnyExpression(arg);
+                }
+
+                output += ")";
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
--- Lang/Unparser/Composite/Expression/UXExpression.cs
+++ Lang/Unparser/Composite/Expression/UXExpression.cs
@@ -23,6 +23,7 @@ namespace GoPowered.Lang.Unparser
 
                  if (HandleSingular(expr.Target, ref output));
             else if (HandleClosure(expr.Target, ref output));
+            else if (HandleMake(expr.Target, ref output));
             else
                 throw new UnparserError("Unexpected expression target '" + TypeOf(expr.Target).Substring(3) + "'");
 
@@ -30,6 +31,7 @@ namespace GoPowered.Lang.Unparser
         }
 
         protected partial bool HandleClosure(IExpressionTarget target, ref string output);
+        protected partial bool HandleMake(IExpressionTarget target, ref string output);
 
         protected bool HandleSingular(IExpressionTarget target, ref string output)
         {
