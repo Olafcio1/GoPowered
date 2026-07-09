ffa842b0eccd47a8a32968962bae841d3bb71ee9
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-07-09 16:52:55 +0200

    [Unparser] Any-expression unparsing boilerplate

---
+++ Lang/Unparser/Composite/UXAnyExpression.cs
@@ -0,0 +1,17 @@
+﻿using GoPowered.Lang.Parser;
+using GoPowered.Lang.Parser.Token;
+using GoPowered.Lang.Parser.Token.Expr;
+
+namespace GoPowered.Lang.Unparser
+{
+    public partial class Unparser
+    {
+        protected string HandleAnyExpression(IAnyExpression anyExpr)
+        {
+            if (anyExpr is Expression expr)
+                return HandleExpression(expr);
+            else
+                throw new UnparserError("Expected an expression");
+        }
+    }
+}
