84f822a382a57e8cd4c39cf8db16e51a952e1bf6
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-07-29 04:18:00 +0200

    [Parser] Constant types

--- Lang/Parser/Composite/Statement/Implementation/PXStmtConst.cs
+++ Lang/Parser/Composite/Statement/Implementation/PXStmtConst.cs
@@ -1,5 +1,6 @@
 ﻿using GoPowered.Lang.Lexer.Token;
 using GoPowered.Lang.Parser.Token.Statement.Implementation;
+using GoPowered.Lang.Parser.Type;
 
 namespace GoPowered.Lang.Parser
 {
@@ -7,12 +8,19 @@ namespace GoPowered.Lang.Parser
     {
         private partial StmtConst ParseConst()
         {
+            IType? type = null;
+
+            if (!Now([("literal", null)]))
+                type = ParseType();
+
             var name = Consume<LTLiteral>().Value;
             Now([("newline", null)], consume: true);
+
             Require(Operator.Set.ToToken(), "'='");
+
             var value = ParseExpression(constant: true);
 
-            return new StmtConst(name, value!);
+            return new StmtConst(name, type, value!);
         }
     }
 }
--- Lang/Parser/Token/Statement/Implementation/StmtConst.cs
+++ Lang/Parser/Token/Statement/Implementation/StmtConst.cs
@@ -1,7 +1,9 @@
-﻿namespace GoPowered.Lang.Parser.Token.Statement.Implementation
+﻿using GoPowered.Lang.Parser.Type;
+
+namespace GoPowered.Lang.Parser.Token.Statement.Implementation
 {
     /**
      * Used to define new constants.
      */
-    public record StmtConst(string Name, IAnyExpression Value) : IStatement;
+    public record StmtConst(string Name, IType Type, IAnyExpression Value) : IStatement;
 }
