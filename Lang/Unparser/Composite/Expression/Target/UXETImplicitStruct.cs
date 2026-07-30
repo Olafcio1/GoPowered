b53ff99cc515fa6be1dfcbb55b13e2955f77a8a9
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-07-30 20:22:24 +0200

    [Unparser] [UXExpression] Unparsing 'ETImplicitStruct'

---
+++ Lang/Unparser/Composite/Expression/Target/UXETImplicitStruct.cs
@@ -0,0 +1,44 @@
+﻿using GoPowered.Lang.Parser.Token.Expr;
+using GoPowered.Lang.Parser.Token.Expr.Target;
+
+namespace GoPowered.Lang.Unparser
+{
+    public partial class Unparser
+    {
+        protected partial bool HandleImplicitStruct(IExpressionTarget target, ref string output)
+        {
+            if (target is ETImplicitStruct cast)
+            {
+                output += "{";
+
+                bool first = true;
+
+                foreach (var parameter in cast.Positional)
+                {
+                    if (first)
+                        first = false;
+                    else output += ", ";
+
+                    output += HandleAnyExpression(parameter);
+                }
+
+                foreach (var (key, parameter) in cast.Keyword)
+                {
+                    if (first)
+                        first = false;
+                    else output += ", ";
+
+                    output += key;
+                    output += ": ";
+                    output += HandleAnyExpression(parameter);
+                }
+
+                output += "}";
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
