93e6cb1c3ab7e17b8cf39bec0878eecaeaa04e9d
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-07-30 21:24:42 +0200

    [PoweredUnparser] Handling type objects

--- PoweredLang/PoweredUnparser/PoweredUnparser.cs
+++ PoweredLang/PoweredUnparser/PoweredUnparser.cs
@@ -1,10 +1,38 @@
 ﻿using GoPowered.Lang.Parser;
 using GoPowered.Lang.Unparser;
+using GoPowered.PoweredLang.PoweredParser.Token.Object;
 
 namespace GoPowered.PoweredLang.PoweredUnparser
 {
     public class PoweredUnparser(Parser input)
                : Unparser(input)
     {
+        protected override void Unparse1(IParserToken tok)
+        {
+            if (tok is PTTypeObject obj)
+            {
+                HandleTypeObject(obj);
+            }
+            else
+            {
+                base.Unparse1(tok);
+            }
+        }
+
+        protected void HandleTypeObject(PTTypeObject typeObject)
+        {
+            output += "\n";
+            output += "type ";
+            output += typeObject.Name;
+
+            if (typeObject.Generics != null)
+                output += HandleFuncGenerics(typeObject.Generics);
+
+            output += " ";
+            output += "struct {";
+            output += HandleInherits(typeObject.Inherits);
+            output += HandleFields(typeObject.Fields);
+            output += "\n}";
+        }
     }
 }
