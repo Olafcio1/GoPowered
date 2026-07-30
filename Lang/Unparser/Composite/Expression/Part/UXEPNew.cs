b4bf9b823f8eae66d3d7d9b4d9713fa2be7f159f
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-07-30 20:13:14 +0200

    [Unparser] [UXExpression] Unparsing 'EPNew'

---
+++ Lang/Unparser/Composite/Expression/Part/UXEPNew.cs
@@ -0,0 +1,44 @@
+﻿using GoPowered.Lang.Parser.Token.Expr;
+using GoPowered.Lang.Parser.Token.Expr.Part;
+
+namespace GoPowered.Lang.Unparser
+{
+    public partial class Unparser
+    {
+        protected partial bool HandleNew(IExpressionPart part, ref string output)
+        {
+            if (part is EPNew @new)
+            {
+                output += "{";
+
+                bool first = true;
+
+                foreach (var parameter in @new.Positional)
+                {
+                    if (first)
+                        first = false;
+                    else output += ", ";
+
+                    output += HandleAnyExpression(parameter);
+                }
+
+                foreach (var (key, parameter) in @new.Keyword)
+                {
+                    if (first)
+                        first = false;
+                    else output += ", ";
+
+                    output += key;
+                    output += ": ";
+                    output += HandleAnyExpression(parameter);
+                }
+
+                output += "}";
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
